e3e96c18ddab7224f86b7d3516065979b84cf729
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2024-05-19 20:19:19 +0000

    sign in widget

--- src/components/auth/layout.tsx
+++ src/components/auth/layout.tsx
@@ -57,7 +57,7 @@ export function Layout({ children }: LayoutProps): React.JSX.Element {
             <Box
               component="img"
               alt="Widgets"
-              src="/assets/auth-widgets.png"
+              src="/assets/auth-widgets.jpg"
               sx={{ height: 'auto', width: '100%', maxWidth: '600px' }}
             />
           </Box>
