2d683fd6b1e1b2179ae58847bb910d068dc14812
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2024-05-19 21:22:41 +0000

    Overview - traffic data

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -40,7 +40,7 @@ export default function Page(): React.JSX.Element {
         />
       </Grid>
       <Grid lg={4} md={6} xs={12}>
-        <Traffic chartSeries={[63, 15, 22]} labels={['Desktop', 'Tablet', 'Phone']} sx={{ height: '100%' }} />
+        <Traffic chartSeries={[63, 15, 22]} labels={['Cows', 'Bulls', 'Calves']} sx={{ height: '100%' }} />
       </Grid>
       <Grid lg={4} md={6} xs={12}>
         <LatestProducts
--- src/components/dashboard/overview/traffic.tsx
+++ src/components/dashboard/overview/traffic.tsx
@@ -29,7 +29,7 @@ export function Traffic({ chartSeries, labels, sx }: TrafficProps): React.JSX.El
 
   return (
     <Card sx={sx}>
-      <CardHeader title="Traffic source" />
+      <CardHeader title="Animal Category" />
       <CardContent>
         <Stack spacing={2}>
           <Chart height={300} options={chartOptions} series={chartSeries} type="donut" width="100%" />
