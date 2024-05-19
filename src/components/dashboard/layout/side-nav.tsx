ec7f34966d73fadd1f98691c9da900224509c142
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2024-05-19 20:27:10 +0000

    Sidebar - Edited bottom

--- src/components/dashboard/layout/side-nav.tsx
+++ src/components/dashboard/layout/side-nav.tsx
@@ -82,14 +82,7 @@ export function SideNav(): React.JSX.Element {
       </Box>
       <Divider sx={{ borderColor: 'var(--mui-palette-neutral-700)' }} />
       <Stack spacing={2} sx={{ p: '12px' }}>
-        <div>
-          <Typography color="var(--mui-palette-neutral-100)" variant="subtitle2">
-            Need more features?
-          </Typography>
-          <Typography color="var(--mui-palette-neutral-400)" variant="body2">
-            Check out our Pro solution template.
-          </Typography>
-        </div>
+        
         <Box sx={{ display: 'flex', justifyContent: 'center' }}>
           <Box
             component="img"
