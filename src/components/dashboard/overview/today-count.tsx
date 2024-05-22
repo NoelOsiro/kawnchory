103ecdb1ffa5529b343ddf5ba26296b2cd721375
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2024-05-22 04:37:23 +0300

    Dev (#5)
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * template literals build fix
    
    * worflow
    
    * Update main.yml
    
    * paths changed, added animals page
    
    * focused on counting module

---
+++ src/components/dashboard/overview/today-count.tsx
@@ -0,0 +1,58 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Stack from '@mui/material/Stack';
+import type { SxProps } from '@mui/material/styles';
+import Typography from '@mui/material/Typography';
+import { ArrowDown as ArrowDownIcon } from '@phosphor-icons/react/dist/ssr/ArrowDown';
+import { ArrowUp as ArrowUpIcon } from '@phosphor-icons/react/dist/ssr/ArrowUp';
+import { Calculator as CalculatorIcon } from '@phosphor-icons/react/dist/ssr/Calculator';
+
+export interface CountProps {
+  diff?: number;
+  trend: 'up' | 'down';
+  sx?: SxProps;
+  value: string;
+}
+
+export function Count({ diff, trend, sx, value }: CountProps): React.JSX.Element {
+  const TrendIcon = trend === 'up' ? ArrowUpIcon : ArrowDownIcon;
+  const trendColor = trend === 'up' ? 'var(--mui-palette-success-main)' : 'var(--mui-palette-error-main)';
+
+  return (
+    <Card sx={sx}>
+      <CardContent>
+        <Stack spacing={3}>
+          <Stack direction="row" sx={{ alignItems: 'flex-start', justifyContent: 'space-between' }} spacing={3}>
+            <Stack spacing={1}>
+              <Typography color="text.secondary" variant="overline">
+                Count
+              </Typography>
+              <Stack direction="row" spacing={1}>
+              <Typography variant="h4">{value}</Typography>
+              <Typography variant="h5">/ 1,210</Typography>
+              </Stack>
+            </Stack>
+            <Avatar sx={{ backgroundColor: 'var(--mui-palette-primary-main)', height: '56px', width: '56px' }}>
+              <CalculatorIcon fontSize="var(--icon-fontSize-lg)" />
+            </Avatar>
+          </Stack>
+          {diff ? (
+            <Stack sx={{ alignItems: 'center' }} direction="row" spacing={2}>
+              <Stack sx={{ alignItems: 'center' }} direction="row" spacing={0.5}>
+                <TrendIcon color={trendColor} fontSize="var(--icon-fontSize-md)" />
+                <Typography color={trendColor} variant="body2">
+                  {diff}%
+                </Typography>
+              </Stack>
+              <Typography color="text.secondary" variant="caption">
+                Since yesterday
+              </Typography>
+            </Stack>
+          ) : null}
+        </Stack>
+      </CardContent>
+    </Card>
+  );
+}
